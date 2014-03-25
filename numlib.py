da3cef9557ff8ec44e3cf360291c9e6279cb75db
Zack <zack.goldberg@trendrr.com>
2014-03-25 16:40:40 -0400

    hotfix numlib content was duplicated

--- numlib.py
+++ numlib.py
@@ -1,4 +1,5 @@
-# from primelib import primeFactorization as factorize
+# from primelib import factorize
+import primelib
 
 def greatestCommonDenominator(a,b):
 	a,b = abs(a), abs(b)
@@ -31,7 +32,7 @@ lcm = leastCommonMultiple
 def eulersTotient(n):
 	if n < 2:
 		return 0
-	primeFactors = set(factorize(n))
+	primeFactors = set(primelib.factorize(n))
 	
 #	given prime factor x of n, (n/x) many integers between 1 and n are also divisble
 #	by x and therefor not coprime with n
@@ -42,54 +43,8 @@ def eulersTotient(n):
 totient = eulersTotient
 phi = eulersTotient
 
-<<<<<<< HEAD
+print phi(11)
 
-# from primelib import primeFactorization as factorize
-
-def greatestCommonDenominator(a,b):
-	a,b = abs(a), abs(b)
-	a,b = max(a,b), min(a,b)
-	if b == 0 or b == a:
-		return a
-	else:
-		return gcd(a % b, b)
-
-gcd = greatestCommonDenominator
-
-
-def coprime(a,b):
-	return (greatestCommonDenominator(a,b) == 1)
-
-isCoprime = coprime
-relativelyPrime = coprime
-relprime = coprime
-
-
-def leastCommonMultiple(a,b):
-	if a == b == 0:
-		return 0
-	else:
-		return (a * b) / greatestCommonDenominator(a,b)
-
-lcm = leastCommonMultiple
-
-#number of integers i, 0 < i < n, that are coprime with n
-def eulersTotient(n):
-	if n < 2:
-		return 0
-	primeFactors = set(factorize(n))
-	
-#	given prime factor x of n, (n/x) many integers between 1 and n are also divisble
-#	by x and therefor not coprime with n
-	for x in primeFactors:
-		n -= (n / x)
-	return n
-
-totient = eulersTotient
-phi = eulersTotient
-
-=======
->>>>>>> numlib
 #modular exponentiation by squaring
 def modularExponentiation(base,exponent,modulus):
 	#normalize base
