96839969fd35e71d13db60a2b8a80ea589bf8014
Zack <zack.goldberg@trendrr.com>
2014-03-27 16:25:08 -0400

    setlib: perm generator accepts allPermuations kwarg, add explanation to nextPerm comments

--- setlib.py
+++ setlib.py
@@ -1,12 +1,11 @@
-from bitlib import nextGreatestSameBitcount
-nextMaskGreater = nextGreatestSameBitcount
+import bitlib
 
 def allMasksSizeK(k,arraysize):
 	masks = []
 	mask = (1 << k)-1
 	while mask <= (1<<arraysize):
 		masks.append(mask)
-		mask = nextGreatestSameBitcount(mask)
+		mask = bitlib.nextGreatestSameBitcount(mask)
 	return masks
 
 def subset(array,mask):
@@ -19,13 +18,16 @@ def subset(array,mask):
 		i += 1
 	return subset
 
-def allSubsets(array):
+#All non-empty subsets
+def allSubsets(array, nullSet=False):
 	length = len(array)
 	mask = (1 << length) - 1
 	subsets = []
 	while mask > 0:
 		subsets.append(subset(array,mask))
 		mask -= 1
+	if nullSet:
+		subsets.append([])
 	return subsets
 powerset = allSubsets
 pset = allSubsets
@@ -65,58 +67,84 @@ def nextPermutationString(strng):
 #returns -1 when no more permutations
 def nextPermutation(iterable):
 	if not isinstance(iterable,list):
+		
 		if isinstance(iterable, (str, unicode)):
 			return nextPermutationString(iterable)
-		if isinstance(iterable,tuple):
-			return tuple(nextPermutation([x for x in iterable]))
+		
+		elif isinstance(iterable,tuple):
+			return tuple(nextPermutation(list(iterable)))
+		
 		else:
-			print "can't give next permutation for this type"
+			print "can't give nextPerm permutation for this type"
 			exit(1)
+
 	length = len(iterable)
+
+	#How many elements at the end are in reverse sorted order. Let's call this the suffix
+	# e.g. for list [1, 3, 5, 7, 9, 8, 6, 4, 2] the suffix includes
+	# elements 9, 8, 6, 4, 2. The last element before the suffix, 7 will be 
+	# swapped with an element from the suffix. Becuase we are considering
+	# permutations that are greater, we choose the smallest element of the 
+	# suffix that is greater than 7 - in this case 8 - and we swap these two
+	# elements.
+
+	#We now have the list [1, 3, 5, 8, 9, 7, 6, 4, 2], and a suffix
+	# of 9 7 6 4 2, which is still in reverse sorted order.
+	# To generate the 'smallest' permutation beginning with our prefix
+	# 1,2,3,5,8, we simply reverse our suffix so that it is now in sorted order
+
 	suffixIndex = length - 2
-	while suffixIndex > -1 and iterable[suffixIndex] >= iterable[suffixIndex+1]:
+	while suffixIndex > -1 and (iterable[suffixIndex] >= iterable[suffixIndex+1]):
 		suffixIndex -= 1
+
 	if suffixIndex < 0: #happens when iterable is reverse sorted, or has length 0
 		return -1
-	prefixEnding = suffixIndex
-	suffixIndex += 1
+	else:
+		prefixEnding = suffixIndex
+		suffixIndex += 1
+
 	while suffixIndex < length and iterable[suffixIndex] > iterable[prefixEnding]:
 		suffixIndex+=1
 	suffixIndex -= 1
 
-	#Swap prefix-ending for smallest elt bigger than prefix-ending in the suffix
+	#Swap prefix-ending with smallest elt in the suffix that is bigger than prefix-ending
 	temp = iterable[suffixIndex]
-	iterable[suffixIndex] = iterable[prefixEnding]
-	iterable[prefixEnding] = temp
+	iterable[suffixIndex], iterable[prefixEnding] = iterable[prefixEnding], iterable[suffixIndex]
 
-	#return ascending prefix plus the descending suffix reversed
+	#return prefix as is, plus the descending-suffix reversed
 	return iterable[0:prefixEnding+1] + iterable[prefixEnding+1:][::-1]
 
 #Seriously consider how stringFlag and tupleFlag should be handled.
 #Do they need to be keyword arguments?
-def nextPermutationGenerator(iterable):
-	yield iterable #include self
-	stringFlag = 0
-	tupleFlag = 0
+def nextPermutationGenerator(iterable, allPermutations=False):
+	stringFlag = False
+	tupleFlag = False
 	if not isinstance(iterable,list):
 		if isinstance(iterable, (str, unicode)):
-			stringFlag = 1
+			stringFlag = True
 		elif isinstance(iterable,tuple):
-			tupleFlag = 1
+			tupleFlag = True
 		else:
-			print "can't give next permutation for this type"
+			print "can't give nextPerm permutation for this type"
 			exit(1)
-
+	
+	if allPermutations:
+		iterable = sorted(iterable)
+		if stringFlag: 
+			#sorted(some_string) will return a list, we want a string
+			iterable = ''.join(iterable)
+	
+	yield iterable #include self
 	iterable = list(iterable)	
 	while True:
-		next = nextPermutation(iterable)
-		if next == -1:
+		nextPerm = nextPermutation(iterable)
+		if nextPerm == -1:
 			return
-		if stringFlag == 1:
-			yield ''.join(next)
-		elif tupleFlag == 1:
-			yield tuple(next)
+		if stringFlag:
+			yield ''.join(nextPerm)
+		elif tupleFlag:
+			yield tuple(nextPerm)
 		else:
-			yield next
-		iterable = next
+			yield nextPerm
+		iterable = nextPerm
 
